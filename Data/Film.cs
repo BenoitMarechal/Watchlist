1c78b18ace3b5ab759314e2ea80ab48033d6c70f
Benoit <bmarechal@vpauto.eu>
2024-02-14 16:38:23 +0100

    Add project files.

---
+++ Data/Film.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Watchlist.Data
+{
+    public class Film
+    {
+        [Key] public int Id { get; set; }
+        public string? Titre { get; set; }
+        public int Annee { get; set; }
+    }
+}
