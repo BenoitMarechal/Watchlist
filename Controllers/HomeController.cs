c5fcbee3e7d69abbc97f77b7a65542dd562e567f
Benoit <bmarechal@vpauto.eu>
2024-02-14 17:04:16 +0100

    Liste as home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,10 @@ namespace Watchlist.Controllers
 
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "ListeFilms");
+            }
             return View();
         }
 
